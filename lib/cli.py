32acb6b7a14598f27d12a775aac6a7594155ca44
Eileen <eileenburdekin@proton.me>
2023-12-29 21:50:35 -0800

    add number commands for main menu

--- lib/cli.py
+++ lib/cli.py
@@ -33,7 +33,7 @@ def display_title_card():
  ╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝    
    
 {"*" * 58}            
-                   {typer.style("a Python CLI project by @eburdekin", fg=typer.colors.CYAN, bold=True)}
+            {typer.style("a Python CLI project by @eburdekin", fg=typer.colors.CYAN, bold=True)}
                       """
     )
 
@@ -44,29 +44,47 @@ def main():
     menu()
 
 
-# title_card_displayed = False
-
-# while True:
-#     if not title_card_displayed:
-#         display_title_card()
-#         title_card_displayed = True
-
-#     menu()
-
-
 @app.command()
 def menu():
-    typer.echo("Choose an option:")
-    typer.echo("1. Load next song")
-    typer.echo("2. Pause current song")
-    typer.echo("3. Add song to queue")
-    typer.echo("4. Remove song from queue")
-    typer.echo("5. View queue by song")
-    typer.echo("6. View queue by singer")
-    typer.echo("7. View all songs")
-    typer.echo("8. View songs by artist")
-    typer.echo("9. View songs by genre")
-    typer.echo("0. Exit")
+    while True:
+        typer.echo("Choose an option:")
+        typer.echo("1. Load next song")
+        typer.echo("2. Pause current song")
+        typer.echo("3. Add song to queue")
+        typer.echo("4. Remove song from queue")
+        typer.echo("5. View queue by song")
+        typer.echo("6. View queue by singer")
+        typer.echo("7. View all songs")
+        typer.echo("8. View songs by artist")
+        typer.echo("9. View songs by genre")
+        typer.echo("0. Exit")
+
+        choice = typer.prompt("Enter the number of your choice (0-9):", type=int)
+        if choice == 1:
+            load_next_song()
+        elif choice == 2:
+            pause_current_song()
+        elif choice == 3:
+            add_song_command()
+        elif choice == 4:
+            remove_song_command()
+        elif choice == 5:
+            view_queue_command()
+        elif choice == 6:
+            view_up_next_command()
+        elif choice == 7:
+            view_all_songs()
+        elif choice == 8:
+            artist = typer.prompt("Enter artist name:")
+            view_songs_by_artist(artist)
+        elif choice == 9:
+            genre = typer.prompt("Enter genre:")
+            view_songs_by_genre(genre)
+        elif choice == 0:
+            exit_command()
+            break
+        else:
+            typer.echo("Invalid choice. Please enter a number between 0 and 9.")
 
 
 @app.command()
@@ -80,9 +98,9 @@ def pause_current_song():
 
 
 @app.command()
-def add_song_command(
-    song_id: str = typer.Argument(...), singer_name: str = typer.Argument(...)
-):
+def add_song_command():
+    song_id = typer.prompt("Enter song ID: ")
+    singer_name = typer.prompt("Who is singing?: ")
     add_song(song_id, singer_name)
 
 
